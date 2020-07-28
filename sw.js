72f2a29e29ff643ab3df49b870c88855a9595e6e
Caleb Evans <caleb39411@gmail.com>
2020-07-28 14:17:27 +0100

    Initial commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('votetc').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/images/tyler_costello_y7.png',
+       '/images/agpl-v3-logo.svg',
+       '/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js',
+       'LICENCE.txt'
+     ]);
+   })
+ );
+});
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
